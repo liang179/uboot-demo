10ea7bed9eaafda1633b5252a984b1e4dca33520
liang179 <liang17937@gmail.com>
2024-08-10 11:48:18 +0000

    improve

--- build-uboot.sh
+++ build-uboot.sh
@@ -1,8 +1,8 @@
-# sudo apt update
-# sudo apt install -y flex gcc-arm-linux-gnueabi
+sudo apt update
+sudo apt install -y flex gcc-arm-linux-gnueabi
 
-# wget https://ftp.denx.de/pub/u-boot/u-boot-2022.07-rc3.tar.bz2
-# tar -xf u-boot-2022.07-rc3.tar.bz2
+wget https://ftp.denx.de/pub/u-boot/u-boot-2022.07-rc3.tar.bz2
+tar -xf u-boot-2022.07-rc3.tar.bz2
 
 
 cd u-boot-2022.07-rc3
